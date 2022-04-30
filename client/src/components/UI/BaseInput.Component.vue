8893b3690392c82dbfa3f4e07d05c48eedbc25f2
Edward Opoku-Agyemang <oaedward83@gmail.com>
2022-04-30 02:47:53 +0000

    base input text field

--- client/src/components/UI/BaseInput.Component.vue
+++ client/src/components/UI/BaseInput.Component.vue
@@ -1,13 +1,37 @@
-<script setup></script>
+<script setup>
+import { defineProps } from "vue";
+
+defineProps({
+  label: {
+    type: String,
+    default: "",
+  },
+  type: {
+    type: String,
+    default: "text",
+  },
+  placeholder: {
+    type: String,
+    default: "",
+  },
+  modelValue: {
+    type: [String, Number],
+    default: "",
+  },
+});
+</script>
 
 <template>
   <div>
-    <label for="">Label</label>
+    <label for="Input">{{ label }}</label>
     <input
-      type="text"
       class="form-control"
-      placeholder="Username"
-      aria-label="Username"
+      v-bind="$attrs"
+      :type="type"
+      :value="modelValue"
+      :placeholder="placeholder"
+      @input="$emit('update:modelValue', $equipment.target.value)"
+      aria-label="label"
     />
   </div>
 </template>
--- client/src/components/layout/SectionLayout.Component.vue
+++ client/src/components/layout/SectionLayout.Component.vue
@@ -1,22 +1,40 @@
 <template>
-  <section class="section">
+  <section class="section mb-3">
     <div class="section-title mb-3">Basic Information</div>
-    <div class="row">
+    <form class="row g-3">
       <div class="col">
-        <BaseInput />
+        <BaseInput
+          v-model="equipment.number"
+          label="Equipment Number"
+          type="number"
+          placeholder=""
+        />
       </div>
       <div class="col">
-        <BaseInput />
+        <BaseInput
+          v-model="equipment.name"
+          label="Equipment Name"
+          type="text"
+          placeholder="Enter Equipment Name"
+        />
       </div>
       <div class="col">
-        <BaseInput />
+        <BaseInput
+          v-model="equipment.serial"
+          label="Equipment Serial"
+          type="text"
+          placeholder="Enter Equipment Serial"
+        />
       </div>
-    </div>
+    </form>
   </section>
 </template>
 
 <script setup>
 import BaseInput from "@/components/UI/BaseInput.Component.vue";
+import { ref } from "vue";
+
+const equipment = ref({ number: 0 }, { name: "" }, { serial: "" });
 </script>
 
 <style></style>
