a73662991cb84db728d2a83072eccab805876af2
Edward Opoku-Agyemang <oaedward83@gmail.com>
2022-12-12 18:42:29 +0000

    manufacturer and model section created

---
+++ src/client/web/src/components/BIOMD/BIOMD/BIOMD-VENDOR/VENDOR-ManufacturerInfo.vue
@@ -0,0 +1,34 @@
+<template>
+  <Section sectionTitle="Manufacturer Information">
+    <div
+      class="row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-2 g-3 mb-3"
+    >
+      <!-- Manufacturer Information -->
+      <div class="col">
+        <label for="manufacturerList" class="form-label">Manufacturer</label>
+        <input
+          class="form-control"
+          list="manufacturerOptions"
+          id="manufacturerList"
+          placeholder="Search and Select Manufacturer..."
+          autocomplete="off"
+        />
+        <datalist id="manufacturerOptions">
+          <option value="General Electric" />
+        </datalist>
+      </div>
+    </div>
+    <div class="col">
+      <slot />
+    </div>
+  </Section>
+</template>
+
+<script setup>
+import Input from "../BIOMD-UI/UI-Input.vue";
+import Section from "../BIOMD-UI/UI-Section.vue";
+</script>
+
+<style lang="scss" scoped>
+@import "../Style/BIOMD.scss";
+</style>
--- src/client/web/src/components/BIOMD/BIOMD/BIOMD-VENDOR/VENDOR.vue
+++ src/client/web/src/components/BIOMD/BIOMD/BIOMD-VENDOR/VENDOR.vue
@@ -34,7 +34,7 @@ Description: < Describe the application >
             <Btn2
               BtnName="Dashboard"
               backgroundColor="#2A94B6"
-              @click="goBack"
+              @click="goNext"
               class="text-light"
             />
           </span>
@@ -44,6 +44,24 @@ Description: < Describe the application >
       <main>
         <VendorInformation />
         <CustomerService />
+        <ManufacturerInformation>
+          <Btn2
+            BtnName="Add Manufacturer"
+            backgroundColor="#1266F1"
+            :icon="'plus'"
+            @click="changePage('manufacturerInfo')"
+            class="rounded-pill"
+          />
+        </ManufacturerInformation>
+        <ModelInformation>
+          <Btn2
+            BtnName="Add Model"
+            backgroundColor="#1266F1"
+            :icon="'plus'"
+            @click="changePage('modelInfo')"
+            class="rounded-pill"
+          />
+        </ModelInformation>
         <div class="d-flex justify-content-center py-3">
           <!-- btn-green and a-link are custom css for MEMS check base.css -->
           <div class="">
@@ -68,12 +86,22 @@ import { ref, toRefs, provide, computed } from "vue";
 
 import VendorInformation from "../BIOMD-VENDOR/VENDOR-VendorInformation.vue";
 import CustomerService from "../BIOMD-VENDOR/VENDOR-CustomerService.vue";
+import ManufacturerInformation from "../BIOMD-VENDOR/VENDOR-ManufacturerInfo.vue";
+import ModelInformation from "../BIOMD-VENDOR/VENDOR-ModelInfo.vue";
 import Btn from "../BIOMD-UI/UI-Btn.vue";
 import Btn2 from "../BIOMD-UI/UI-Btn2.vue";
 import Header from "../BIOMD-UI/UI-FormHeader.vue";
 
 export default {
-  components: { CustomerService, VendorInformation, Btn2, Btn, Header },
+  components: {
+    CustomerService,
+    VendorInformation,
+    ManufacturerInformation,
+    ModelInformation,
+    Btn2,
+    Btn,
+    Header,
+  },
   name: "vendor",
   // Define Props here
   props: {
@@ -172,6 +200,10 @@ export default {
       emit("updatePage", "landing");
     };
 
+    const changePage = async (page) => {
+      emit("updatePage", page);
+    };
+
     provide("vendorName", vendorName);
     provide("country", country);
     provide("region", region);
@@ -185,6 +217,7 @@ export default {
       getValues,
       // Send Functionality to HTML
       goBack,
+      changePage,
       function_name,
       redirectToPage,
       createRecord,
@@ -197,7 +230,7 @@ export default {
 .RevealContainer {
   min-height: 100vh;
   max-height: 100vh;
-  overflow:scroll;
+  overflow: scroll;
 }
 .RevealContainer::-webkit-scrollbar {
   display: none;
