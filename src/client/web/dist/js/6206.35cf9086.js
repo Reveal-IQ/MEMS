c505a94be1f4e04ab9dfab563403c6aedbc9b2c0
Akshay Puli <akshaypuli@outlook.com>
2023-04-16 12:55:12 -0700

    Release v0.1 Build Files

---
+++ src/client/web/dist/js/6206.35cf9086.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkclient"]=self["webpackChunkclient"]||[]).push([[6206],{6206:function(t,e,s){s.r(e),s.d(e,{default:function(){return r}});var n=s(6252),c=s(3577);const i={class:"section p-4 rounded"},l={class:"pb-3 fsXl section-title"};var o={props:{sectionTitle:{type:String,default:"Section Title"}},setup(t){return(e,s)=>((0,n.wg)(),(0,n.iD)("section",i,[(0,n._)("h5",l,(0,c.zw)(t.sectionTitle),1),(0,n.WI)(e.$slots,"default")]))}},u=s(3744);const a=(0,u.Z)(o,[["__scopeId","data-v-9468e16e"]]);var r=a}}]);
+//# sourceMappingURL=6206.35cf9086.js.map
