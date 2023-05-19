51dad9d4a6460bf905ab669d8a17420548167a3d
Akshay Puli <akshaypuli@outlook.com>
2023-05-18 22:48:38 -0700

    Build Files

---
+++ src/client/web/dist/js/290.b5fb49b7.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkclient"]=self["webpackChunkclient"]||[]).push([[290],{290:function(t,e,l){l.r(e),l.d(e,{default:function(){return p}});var s=l(6252),i=l(3577);const u={class:"title fs2xl"},n={class:"sub-title fsSm"};var r={props:{title:{type:String,default:"Title"},subTitle:{type:String,default:"Sub Title Here"}},setup(t){return(e,l)=>((0,s.wg)(),(0,s.iD)("div",null,[(0,s._)("h1",u,(0,i.zw)(t.title),1),(0,s._)("p",n,(0,i.zw)(t.subTitle),1)]))}},a=l(3744);const c=(0,a.Z)(r,[["__scopeId","data-v-7885740e"]]);var p=c}}]);
+//# sourceMappingURL=290.b5fb49b7.js.map
