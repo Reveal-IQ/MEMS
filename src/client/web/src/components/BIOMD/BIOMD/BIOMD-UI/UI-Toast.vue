694ee915a757c46a2a4f06c94f550bbfb577aed9
Edward Opoku-Agyemang <87245788+oaedward@users.noreply.github.com>
2023-10-15 17:04:15 +0000

    Feature/create UI toast (#107)
    ui toast created

---
+++ src/client/web/src/components/BIOMD/BIOMD/BIOMD-UI/UI-Toast.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    {{ message }}
+  </div>
+</template>
+
+<script setup>
+const propos = defineProps({
+  message: {
+    type: String,
+  },
+});
+</script>
+
+<style lang="scss" scoped></style>
